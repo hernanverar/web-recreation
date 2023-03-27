01e9077f7bd2e9c4cec5e77696823add35792726
hernan verar <hernanverar@gmail.com>
2023-03-27 11:35:08 -0700

    added link and h1 to our page in JS

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const h1 = document.createElement("h1");
 const h2Element = document.createElement ("h2");
 const pElement = document.createElement ("p");
 const img = document.createElement("img");
+const Secondh1 = document.createElement("h1");
 const aElement = document.createElement("a");
 
 img.src="https://cdn.download.ams.birds.cornell.edu/api/v1/asset/202984001/900"
@@ -17,14 +18,16 @@ img.style = "width:50%"
 aElement.href = "https://en.wikipedia.org/wiki/Multicoloured_tanager"
 
 
-h1.append("this is a new header!!");
-pElement.append("this is a <p> tag");
+h1.append("Webpage Recreation Practice");
+pElement.append("The HTML of this webpage was created with JavaScript.");
 h2Element.append("this is a <h2> tag");
-bodyElement.append (h1);
+Secondh1.append("Facts about the Multicolored Tanager");
+bodyElement.prepend(h1)
 bodyElement.append (h2Element);
 bodyElement.append (pElement);
 bodyElement.append (img);
-aElement.append("link")
+aElement.append("link");
+bodyElement.append (Secondh1);
 bodyElement.prepend (aElement);
 
 };
